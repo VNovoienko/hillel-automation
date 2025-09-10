6b6517aed3944d562a243e6906956025c90fb68e
Novoienko <vnovoienko@luxoft.com>
2025-09-10 11:34:38 +0300

    fix const data

---
+++ jsBasics/task1.js
@@ -0,0 +1,14 @@
+const nameSchool = 'Hillel';
+console.log('I study at', nameSchool);
+
+const myAge = 26;
+console.log('I`m ', myAge);
+
+const editForm = true;
+console.log('Ability to change form: ', editForm);
+
+const fieldEmail = null;
+console.log('Value of the email field: ', fieldEmail);
+
+let user;
+console.log(user);
---
+++ jsBasics/task2.js
@@ -0,0 +1,11 @@
+const user1 = 'Vlada';
+const user2 = 'John';
+const hello = 'Greetings to ' + user1 + ' and ' + user2;
+
+console.log(hello);
+
+const greetings = `Greetings to ${user1} and ${user2}`;
+console.log(greetings)
+
+//This expression can be combined into a single line for brevity."
+console.log(`Greetings to ${user1} and ${user2}`);
