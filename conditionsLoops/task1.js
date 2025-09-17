2e8cabdea1fa73382b5cc329c6f9a3135a7fb70b
Novoienko <vnovoienko@luxoft.com>
2025-09-17 21:12:01 +0300

    hw about conditions and loops

---
+++ conditionsLoops/task1.js
@@ -0,0 +1,15 @@
+const averageGrade = 75;
+
+if (averageGrade <= 60) {
+    console.log("Grade: Unsatisfactory")
+} else if (averageGrade <= 70) {
+    console.log("Grade: Satisfactory")
+} else if (averageGrade <= 80) {
+    console.log("Grade: Good")
+} else if (averageGrade <= 90) {
+    console.log("Grade: Very Good")
+} else if (averageGrade <= 100) {
+    console.log("Grade: Excellent")
+} else {
+    console.log("Outstanding")
+}
